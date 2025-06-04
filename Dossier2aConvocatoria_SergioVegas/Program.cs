e2f8cb0ece0d849ff13cdf2b4f3c363a19822414
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-06-04 14:37:47 +0200

    Add project files.

---
+++ Dossier2aConvocatoria_SergioVegas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dossier2aConvocatoria_SergioVegas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
