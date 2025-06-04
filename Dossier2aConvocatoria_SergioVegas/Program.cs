83e6cfbfce4583cc19cacd4adaf7172febb109c5
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-06-04 19:51:29 +0200

    Exercici 8 fet

--- Dossier2aConvocatoria_SergioVegas/Program.cs
+++ Dossier2aConvocatoria_SergioVegas/Program.cs
@@ -1,43 +1,56 @@
-﻿namespace Dossier2aConvocatoria_SergioVegas
+﻿using Dossier2aConvocatoria_SergioVegas.Tools;
+namespace Dossier2aConvocatoria_SergioVegas
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Exercici7();
+            Exercici8();
         }
         //7. Implementa un programa en C# que demani un nombre enter positiu i mostri tots els seus divisors parells. Utilitza la interpolació de cadenes per mostrar el resultat.
         public static void Exercici7()
         {
             const string MsgEx7 = "Introdueix un nombre positiu i et mostraré tots els seus divisors parrells!";
             const string ErrorEx7 = "Aquest no es un nombre positiu! Torna ha introduir un numero.";
-            const string ErrorFormatEx7 = "Format incorrecte. Introdueix un numero positiu.";
-            const string MsgIsEven = "{0} és un nombre par";
             const int Even = 2;
-            bool isCorrectFormat = false;
+
 
             Console.WriteLine(MsgEx7);
 
-            while (!isCorrectFormat)
+            int userNumber = CheckNumbers.CheckValidNumber();
+
+            if (userNumber > 0)
             {
-                try
+                for (int i = 1; i < userNumber; i++)
                 {
-                    int userNumber = int.Parse(Console.ReadLine());
-
-                    if (userNumber > 0) 
-                    {
-                        for (int i = 1; i < userNumber; i++)
-                        {
-                            if (i % Even == 0)
-                            { Console.WriteLine(MsgIsEven, i); }
-                        }
-
-                        isCorrectFormat = true ;
-                    }
-                    else { Console.WriteLine(ErrorEx7); }
+                    if (i % Even == 0)
+                    { Console.WriteLine($"{i} és un nombre par"); }
                 }
-                catch(FormatException) { Console.WriteLine(ErrorFormatEx7); }
             }
+            else { Console.WriteLine(ErrorEx7); }
+
+
+
+        }
+        //8. Modifica el diagrama de fluxe següent (implementa la modificació en un nou diagrama i en C#) perquè calculi la mitjana de N números introduïts per teclat. Mostra el resultat amb composite formatting.	
+        public static void Exercici8()
+        {
+            const string MsgEx8 = "Introdueix N nombres i faré la mitjana";
+            const string MsgEx8Number = "Introdueix un numero";
+            const string MsgAverage = "{0} és la mitja dels numeros introduits";
+
+            Console.WriteLine(MsgEx8);
+
+            int userQuantityNumbers = CheckNumbers.CheckValidNumber();
+            int amount = 0;
+
+            for (int i = 0; i < userQuantityNumbers; i++)
+            {
+                Console.WriteLine(MsgEx8Number);
+                int userNumb = CheckNumbers.CheckValidNumber();
+                amount += userNumb;
+            }
+            Console.WriteLine(MsgAverage, amount / userQuantityNumbers);          
         }
     }
 }
