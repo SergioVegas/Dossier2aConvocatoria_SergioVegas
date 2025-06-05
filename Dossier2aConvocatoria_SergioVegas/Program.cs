71d5cb41ff011e7ba9b83a8f4002ef36f80fb05a
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-06-05 12:48:39 +0200

    Exercici 5 fet

--- Dossier2aConvocatoria_SergioVegas/Program.cs
+++ Dossier2aConvocatoria_SergioVegas/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Dossier2aConvocatoria_SergioVegas.Tools;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
 
 namespace Dossier2aConvocatoria_SergioVegas
 {
@@ -8,8 +7,39 @@ namespace Dossier2aConvocatoria_SergioVegas
     {
         static void Main(string[] args)
         {
-            Tema1.MenuT1();
+            Exercici5();
         }
-        
+        // 5.  Implementa un programa que retorni per pantalla el factorial d’un número introduït per teclat i si aquest és primer o no (de manera iterativa i de manera recursiva).
+        public static void Exercici5()
+        {
+            const string MsgEx5 = "Introdueix un numero no decmial i et retornaré el factorial d'aquest";
+            const string MsgResult = "El factorial del numero introduit és {0}";
+
+            Console.WriteLine(MsgEx5);
+
+            int userNumb = CheckNumbers.CheckValidNumberNoNegative();
+            int result = userNumb;
+
+            for (int i = 1; i < userNumb; i++)//Iterativa
+            {
+                result *= i;
+            }
+            Console.WriteLine(MsgResult,result);
+            Console.WriteLine($"{userNumb} {(NumberIsPrime(userNumb) ? "és un nombre primer." : "no és un nombre primer.")}");
+        }
+        public static int FactorialRecursiu(int userNumb) //Recursiva
+        {
+            if (userNumb <= 1) return 1;
+            return userNumb * FactorialRecursiu(userNumb - 1);
+        }
+        static bool NumberIsPrime(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
     }
 }
--- Dossier2aConvocatoria_SergioVegas/Tools/CheckNumbers.cs
+++ Dossier2aConvocatoria_SergioVegas/Tools/CheckNumbers.cs
@@ -9,6 +9,7 @@ namespace Dossier2aConvocatoria_SergioVegas.Tools
     public class CheckNumbers
     {
         const string MsgErrorFormat = "Format incorrecte. Introdueix un numero.";
+        const string MsgErrorNegative= "El numero no pot ser menor a 0.";
         public static int CheckValidNumber()
         {
             
@@ -20,6 +21,29 @@ namespace Dossier2aConvocatoria_SergioVegas.Tools
                 {
                     userNumb = int.Parse(Console.ReadLine());
                     isCorrectFormat = true;
+                }
+                catch (FormatException) { Console.WriteLine(MsgErrorFormat); }
+            }
+            return userNumb;
+        }
+        public static int CheckValidNumberNoNegative()
+        {
+
+            bool isCorrectFormat = false;
+            int userNumb = 0;
+
+            while (!isCorrectFormat)
+            {
+                try
+                {
+                    userNumb = int.Parse(Console.ReadLine());
+                    isCorrectFormat = true;
+                    if (userNumb <= 0) 
+                    {
+                        isCorrectFormat = false;
+                        Console.WriteLine(MsgErrorNegative);
+                    }
+                   
                 }
                 catch (FormatException) { Console.WriteLine(MsgErrorFormat); }
             }
